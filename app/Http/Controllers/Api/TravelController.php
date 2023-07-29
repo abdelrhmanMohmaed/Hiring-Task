e0af5240b097c8354eb6b7dd08f63f5062a7e9fc
AbdelrahmanMohameed <abdohossam120@gmail.com>
2023-07-29 07:47:59 +0300

    Publish a project
    create a migration and models and make relations and use pages (eloquent-sluggable) to make a slug name.

---
+++ app/Http/Controllers/Api/TravelController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TravelResource;
+use App\Models\Travel;
+use Illuminate\Http\Request;
+
+class TravelController extends Controller
+{
+    public function index()
+    {
+
+        $travels = Travel::where('is_public', True)->paginate(1);
+
+        return TravelResource::collection($travels);
+    }
+}
