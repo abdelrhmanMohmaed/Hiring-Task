0c84843a5c7ff83c1d35581d9c5dd50f794c6a40
AbdelrahmanMohameed <abdohossam120@gmail.com>
2023-08-07 21:28:15 +0300

    Finish Editor Endpoint to Update Travel
    Finish Editor Endpoint to Update Travel

--- routes/api.php
+++ routes/api.php
@@ -28,10 +28,18 @@ Route::prefix('v1')->group(function () {
     Route::get('/travels/{travel:slug}/tours', [TourController::class, 'index']);
 
 
-    Route::prefix('admin')->middleware(['auth:sanctum','role:admin'])->group(function () {
+    Route::prefix('admin')->middleware(['auth:sanctum'])->group(function () {
 
-        Route::post('/travels', [Admin\TravelController::class, 'store']);
-        Route::post('/travels/{travel}/tours', [Admin\TourController::class, 'store']);
+        Route::middleware('role:admin')->group(function () {
+
+            Route::post('/travels', [Admin\TravelController::class, 'store']);
+            Route::post('/travels/{travel}/tours', [Admin\TourController::class, 'store']);
+        });
+
+        Route::middleware('role:editor')->group(function () {
+
+            Route::put('/travels/{travel}/', [Admin\TravelController::class, 'update']);
+        });
     });
 
     Route::post('admin/login', LoginController::class);
